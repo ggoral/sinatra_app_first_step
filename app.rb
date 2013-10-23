59d511ca757202291a7b4173340cb624e453765c
ggoral <goral.gonzalo@gmail.com>
2013-10-23 11:55:33 -0300

    add files

--- app.rb
+++ app.rb
@@ -4,3 +4,12 @@ Bundler.require :default, ENV['RACK_ENV'].to_sym
 get '/' do
   'Hello World'
 end
+
+not_found do
+  'Ruta no encontrada'
+end
+
+error do
+  'Disculpá, ocurrió un error horrible - ' +
+    env['sinatra.error'].name
+end
